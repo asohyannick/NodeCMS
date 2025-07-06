465c9fc537006a50253c46ab99fb9e84864680c7
asohyannick <asohyannick3@gmail.com>
2025-07-06 05:33:12 -0700

    Admin now has the honor to now fetch and view a single user within our NodeCMS backend REST API successfully!

---
+++ src/service/impl/user/showUser/showUser.impl.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import User from "../../../../model/user/user.model";
+const showUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(StatusCodes.NOT_FOUND).json({message: "User doesn't exist!"});
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "User has been retrieved successfully!",
+            user,
+        });
+    } catch (error) {
+        console.error('Error ocurred!', error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error",
+        });
+    }
+}
+
+export default showUser;
