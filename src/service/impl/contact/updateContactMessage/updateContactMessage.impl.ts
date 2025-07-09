8f8903ffc1bdfb6a0cdb89fb190cdbcf87b50bd3
asohyannick <asohyannick3@gmail.com>
2025-07-09 15:14:18 -0700

    User can now edit and update a single available contact message sent to the admin about a single available content created and saved successfully on our NodeCMS application backend REST API

---
+++ src/service/impl/contact/updateContactMessage/updateContactMessage.impl.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Contact from "../../../../model/contact/contact.model";
+const updateContactMessage = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.params;
+        const {
+            name,
+            email,
+            subject,
+            message,
+        } = req.body;
+        const updateMessage = await Contact.findByIdAndUpdate(id, {
+            name,
+            email,
+            subject,
+            message,
+            isResponded: true,
+        }, { new: true, runValidators: true });
+        if (!updateMessage) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: "Contact message doesn't exist!" })
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Contact messsage has been updated successfully!",
+            updateMessage,
+        });
+    } catch (error) {
+        console.error("Error occured!", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error",
+        });
+    }
+}
+
+export default updateContactMessage;
