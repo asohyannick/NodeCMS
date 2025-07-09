de703a8434be2ec139a9e8ab09069c1be6b62aeb
asohyannick <asohyannick3@gmail.com>
2025-07-09 08:17:37 -0700

    Admin can now delete or removed permanently a single available content category successfully on our NodeCMS application backend REST API

---
+++ src/service/impl/category/deleteCategory/deleteCategory.impl.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Category from "../../../../model/category/category.model";
+const deleteCategory = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.params;
+        const category = await Category.findByIdAndDelete(id);
+        if (!category) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: "Content category doesn't exist!"})
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Content category has been deleted successfully!",
+            category,
+        });
+    } catch (error) {
+        console.error("Error occured!", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error",
+        })
+    }
+}
+
+export default deleteCategory;
